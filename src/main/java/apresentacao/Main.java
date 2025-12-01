830b042be0271177aa24a10a3eadf7523ae443b2
Lodetti <135389052+Lodetti@users.noreply.github.com>
2025-12-01 20:50:11 -0300

    Configuracao do javapath

--- src/main/java/apresentacao/Main.java
+++ src/main/java/apresentacao/Main.java
@@ -27,7 +27,7 @@ public class Main {
     public static void main(String[] args) {
 
     try {
-        sistema = new Sistema("#Arthur123");
+        sistema = new Sistema("MinhaSenha");
 
         System.out.println("Escolha a oopcao:");
         int opcao = -1;
